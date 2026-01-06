1efcea3478c549965c11131763666a3db256e9cd
benjamin lopez <benjaminlopezzigaran01@gmail.com>
2026-01-06 16:53:14 -0300

    railway

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,7 +6,10 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    preview: {
+      allowedHosts: true
+    }
   },
   integrations: [react()],
   i18n: {
