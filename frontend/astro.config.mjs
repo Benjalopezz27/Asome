0efa25d746950f3328695cece4a69eb7347d8934
benjamin lopez <benjaminlopezzigaran01@gmail.com>
2026-01-09 12:45:12 -0300

    metatags

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,21 +3,30 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     preview: {
-      allowedHosts: ['asome-front-production.up.railway.app']
+      allowedHosts: ['asomelab.com']
     }
   },
-  integrations: [react()],
-  i18n: {
-    defaultLocale: 'es-AR',
-    locales: ['es-AR', 'en'],
-    routing: {
-      prefixDefaultLocale: true,
-      redirectToDefaultLocale: true
+  integrations: [react(), sitemap(
+    {
+      i18n: {
+        defaultLocale: 'es-AR',
+        locales: {
+          'es-AR': 'es-AR',
+          'en': 'en',
+        }
+      },
+      // @ts-ignore
+      routing: {
+        prefixDefaultLocale: true,
+        redirectToDefaultLocale: true
+      }
     }
-  },
+  )],
+  
 });
